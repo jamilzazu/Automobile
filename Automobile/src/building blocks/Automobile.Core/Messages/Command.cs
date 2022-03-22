d03195250a889528556d81e72552a526b43cbc4f
Jamil Zazu <jamil.zazu@amaggi.com.br>
2022-03-22 10:00:12 -0400

    feat: Adicionando validação de comandos

--- Automobile/src/building blocks/Automobile.Core/Messages/Command.cs
+++ Automobile/src/building blocks/Automobile.Core/Messages/Command.cs
@@ -7,12 +7,12 @@ namespace Automobile.Core.Messages
     public abstract class Command : Message, IRequest<ValidationResult>
     {
         public DateTime Timestamp { get; private set; }
-        public ValidationResult ValidationResult { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
 
 
         protected Command()
         {
-            Timestamp = DateTime.Now; 
+            Timestamp = DateTime.Now;
         }
 
         public virtual bool EhValido()
