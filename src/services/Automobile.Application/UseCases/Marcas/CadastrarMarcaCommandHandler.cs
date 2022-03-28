c4ad4dccfe6aa8782c93e60474b831b04a06612b
Jamil Zazu <jamil.zazu@amaggi.com.br>
2022-03-27 22:39:22 -0400

    feat: Implementando testes em command Marcas

--- src/services/Automobile.Application/UseCases/Marcas/CadastrarMarcaCommandHandler.cs
+++ src/services/Automobile.Application/UseCases/Marcas/CadastrarMarcaCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using Automobile.Core.Enums;
+﻿using Automobile.Application.Queries.Request;
+using Automobile.Application.Services.Interfaces;
+using Automobile.Core.Enums;
 using Automobile.Core.Messages;
 using Automobile.Domain.Commands.Marca;
 using Automobile.Domain.Entitites;
@@ -13,21 +15,23 @@ namespace Automobile.Domain.Handlers.Marcas
     public class CadastrarMarcaCommandHandler : CommandHandler, IRequestHandler<CadastrarMarcaCommand, ValidationResult>
     {
         private readonly IMarcaRepository _marcaRepository;
+        private readonly IMarcaService _marcaService;
 
-        public CadastrarMarcaCommandHandler(IMarcaRepository marcaRepository)
+        public CadastrarMarcaCommandHandler(IMarcaRepository marcaRepository, IMarcaService marcaService)
         {
             _marcaRepository = marcaRepository;
+            _marcaService = marcaService;
         }
 
         public async Task<ValidationResult> Handle(CadastrarMarcaCommand message, CancellationToken cancellationToken)
         {
             if (!message.EhValido()) return message.ValidationResult;
 
-            //if (MarcaJaExiste(message.nome))
-            //{
-            //    AdicionarErro($"Este {message.Documento.TipoDocumentoDescricao()} já está em uso.");
-            //    return ValidationResult;
-            //}
+            if (MarcaJaExiste(message.Nome))
+            {
+                AdicionarErro($"A marca {message.Nome} já está existe.");
+                return ValidationResult;
+            }
 
             var marca = MontaObjetoMarca(message);
 
@@ -41,12 +45,14 @@ namespace Automobile.Domain.Handlers.Marcas
             return new Marca(message.Id, message.Nome, Cancelado.Nao);
         }
 
-        //public bool MarcaJaExiste(string nomeMarca)
-        //{
-        //    var marcaJaExiste = _marcaRepository;
+        public bool MarcaJaExiste(string nomeMarca)
+        {
+            var filtro = new FiltroListaMarcasRequest { Busca = nomeMarca };
 
-        //    return marcaJaExiste.Result != null;
-        //}
+            var marcaJaExiste = _marcaService.ListarMarcas(filtro).Data;
+
+            return marcaJaExiste != null;
+        }
 
         public void CadastrarMarca(Marca marca)
         {
