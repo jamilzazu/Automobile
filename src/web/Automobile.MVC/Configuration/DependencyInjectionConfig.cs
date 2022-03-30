e9cf46b0170fcdd4d1778d9d56c1c4dc6dc5102f
Jamil Zazu <jamil.zazu@amaggi.com.br>
2022-03-30 11:46:00 -0400

    feat: Criando projeto MVC

---
+++ src/web/Automobile.MVC/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,49 @@
+﻿using Automobile.MVC.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using System;
+using System.Net.Http;
+
+namespace Automobile.MVC.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region HttpServices
+
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
+            #endregion
+        }
+    }
+
+    #region PollyExtension
+
+    public static class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10),
+                }, (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            return retry;
+        }
+    }
+
+    #endregion
+}
---
+++ src/web/Automobile.MVC/Services/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Automobile.MVC.Services
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            ////var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+            //if (!string.IsNullOrEmpty(authorizationHeader))
+            //{
+            //    request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+            //}
+
+            //var token = _user.ObterUserToken();
+
+            //if (token != null)
+            //{
+            //    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            //}
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
