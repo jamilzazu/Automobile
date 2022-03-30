4619daa3bcb04b95804bf6ecd2adb7d7cdda17c9
Jamil Zazu <jamil.zazu@amaggi.com.br>
2022-03-30 07:10:13 -0400

    feat: Adicionando projeto MVC

---
+++ src/web/Automobile.MVC/Extensions/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Automobile.MVC.Extensions
+{
+    public class AppSettings
+    {
+        public string AutomobileUrl { get; set; }
+    }
+}
---
+++ src/web/Automobile.MVC/Services/AutomobileService.cs
@@ -0,0 +1,19 @@
+﻿using Automobile.MVC.Extensions;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net.Http;
+
+namespace Automobile.MVC.Services
+{
+
+    public class AutomobileService : Service
+    {
+        private readonly HttpClient _httpClient;
+
+        public AutomobileService(HttpClient httpClient, IOptions<AppSettings> settings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(settings.Value.AutomobileUrl);
+        }
+    }
+}
